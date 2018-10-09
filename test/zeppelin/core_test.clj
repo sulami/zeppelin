aa501c63290092dbc5f9e43837f1c4ed328b68d2
Robin Schroer <sulami@peerwire.org>
2018-10-09 16:54:00 +0200

    Fix a test

--- test/zeppelin/core_test.clj
+++ test/zeppelin/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [zeppelin.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest sanity-test
+  (testing "it is sane"
+    (is (= 1 1))))
